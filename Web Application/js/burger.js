5f027d35af28cc9655edb24c41e846b195b9db59
Sanuth23 <sanuth0000@gmail.com>
2023-12-20 00:31:39 +0530

    item page

---
+++ Web Application/js/burger.js
@@ -0,0 +1,10 @@
+// function myFunction() {
+//     let x = document.getElementById("submarine");
+
+//     if(x.style.display==='none'){
+//         x.style.display='block'
+        
+//     }else{
+//         x.style.display='none'
+//     }
+//   }
